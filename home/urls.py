a4143f208cf629a6cd3dff8fc7d861d687d45267
Nebir Kiron <kironnibir046@gmail.com>
2020-07-18 08:57:06 +0600

    modyfied user authentication login logout

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),        
-    path('search/',views.search,name='search'),        
+    path('search/',views.search,name='search'),       
+    path('signup/',views.handlesignup,name='handlesignup'),
+    path('login/',views.handlelogin,name='handlelogin'),
+    path('logout/',views.handlelogout,name='handlelogout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import Contact
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User 
 from blog.models import Post
 
 # Create your views here.
@@ -28,14 +30,14 @@ def contact(request):
 def search(request):
     query = request.GET['search']
 
-    if len(query) > 80:
+    if len(query) > 80 or len(query) < 3:
         allpost = Post.objects.none()
     else:    
         allposttitle = Post.objects.filter(title__icontains=query)
         allpostcontent = Post.objects.filter(content__icontains=query)
         allpost = allposttitle.union(allpostcontent)
 
-    if allpost.count()==0:
+    if allpost.count() == 0:
         messages.warning(request, 'No search results found..please refine your query')
         
     contex = {
@@ -43,3 +45,66 @@ def search(request):
         'query':query,
     }
     return render(request,'home/search.html',contex)
+
+def handlesignup(request):
+    if request.method == "POST":
+        # get the post values
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+
+        # check all the requiements
+        if len(username) > 10:
+            messages.error(request,'username must be under 10 character')
+            return redirect('/')
+
+        if not username.isalnum():
+            messages.error(request,'username should contains only character and number')
+            return redirect('/')            
+
+        if pass1 != pass2:
+            messages.error(request,'password doesn\'t match')
+            return redirect('/')
+
+
+
+        # create user
+        user = User.objects.create_user(username , pass1 , email)
+        user.first_name = fname
+        user.last_name = lname
+        user.save()
+        messages.success(request,'You have created account successfully') 
+        return redirect('/')
+    
+    else:
+        return HttpResponse('404 -- Not found')
+
+def handlelogin(request):
+    if request.method == "POST":
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+    
+        user = authenticate(username = loginusername, password = loginpassword)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,'successfully loged in')
+            return redirect('/')
+        else:
+            messages.error(request,'username or password doese not match')
+            return redirect('/')
+
+    return HttpResponse('404 -- Not found')
+
+def handlelogout(request):
+    
+    logout(request)
+    messages.error(request,'successfully loged out')
+    return redirect('/')
+
+
+    
